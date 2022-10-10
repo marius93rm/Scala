f3710a04692fd8fb3331061d06f8e19c4213d227
marius93rm <marius93rm@gmail.com>
2022-10-10 09:52:47 +0200

    es doppie

---
+++ doppie.scala
@@ -0,0 +1,9 @@
+def isDoppie(stringa:String ): Boolean = {
+    // ...
+}
+
+@main def run() =
+    isDoppie("ciao") // false
+    isDoppie("doppie") // true
+    isDoppie("doppiedoppie") // true
+    isDoppie("melee") // true
