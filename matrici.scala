e27dae23d5e1d40c67790c4c515e043b47b39cd6
marius93rm <marius93rm@gmail.com>
2022-10-11 16:42:23 +0200

    soluzione matrice random

--- matrici.scala
+++ matrici.scala
@@ -46,15 +46,74 @@ def mediaDegliElementi(matrice: Array[Array[Int]]):Unit = {
     }
     println()
   }
+}
 
+def nuovaStampa(matrice: Array[Array[Int]]): Unit = {
+  //media
+  
+  var somma:Float = 0
+  val dim = matrice.length
+  
+  for (i <- 0 to matrice.length-1) {
+    for (j <- 0 to matrice(i).length-1) {
+      somma += matrice(i)(j)
+    }
+  }
+  val media = somma/(matrice.length*matrice.length)
+  println("La media degli elementi della matrice è: " + media)
+
+  val risultato = Array.ofDim[Int](dim,dim)
+
+  var x = 0
+  var y = 0
+
+  for (i <- 0 until dim) {
+    for (j <- 0 until dim) {
+      if(matrice(i)(j) > media) {
+        risultato(x)(y) = matrice(i)(j)
+        if(y < dim-1) {
+          y += 1
+        }
+        else {
+          y = 0
+          x += 1
+        }
+      }
+      else {
+        //niente
+      }
+    }
+  }
+
+  for (i <- x until dim) {
+    if (i==x){
+      for (j <- y until dim) {
+        risultato(i)(j) = 1
+      }
+    }
+    else {
+      for (j <- 0 until dim) {
+        risultato(i)(j) = 1
+      }
+    }  
+  }
+
+
+  //stampa risutlato
+  for (i <- 0 until dim) {
+    for (j <- 0 until dim) {
+      print(risultato(i)(j) + "  ")
+    }
+    println()
+  }
 
 }
 
 @main def run() =
-  // matriceRandom(10,10)
+  // // matriceRandom(10,10)
   
   //creo random
-  val dim = Random.between(2, 10)
+  val dim = Random.between(8, 20)
   //matrice quadrata
   var mioArray = Array.ofDim[Int](dim,dim)
   //ciclo le righe
@@ -75,5 +134,11 @@ def mediaDegliElementi(matrice: Array[Array[Int]]):Unit = {
 
   println("-----------")
 
-  //stampo la matrice
-  mediaDegliElementi(mioArray)
+  nuovaStampa(mioArray)
+
+  // //stampo la matrice
+  // mediaDegliElementi(mioArray)
+
+  // val nome = "Marius"
+  // val car = nome.charAt(0)
+  // println((car+4).toChar)
