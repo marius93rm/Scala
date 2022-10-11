01c06714d2ff6aea20c139f93b849babcadd84fd
marius93rm <marius93rm@gmail.com>
2022-10-11 17:19:26 +0200

    no lettere straniere

--- cesare.scala
+++ cesare.scala
@@ -1,11 +1,11 @@
-
 def criptaCesare(frase: String, key: Int): String = {
-    val dizionario = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+    val dizionario = ('a','b','c','d','e','f','g','h','i','l','m','n','o','p','q','r','s','t','u','v','z')
 
 }
 
 def decriptaCesare(frase: String, key: Int): String = {
-    val dizionario = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+    val dizionario = ('a','b','c','d','e','f','g','h','i','l','m','n','o','p','q','r','s','t','u','v','z')
+
 
 }
 
@@ -23,4 +23,6 @@ def decriptaCesare(frase: String, key: Int): String = {
     println(chiaro)
     print("Frase criptata: ")
     println(criptaCesare(chiaro, chiave))
+
+
 
