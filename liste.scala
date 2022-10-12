3a67cd3f9a94cf69469cee7ea49ca311c725349f
marius93rm <marius93rm@gmail.com>
2022-10-12 10:14:45 +0200

    3 es

--- liste.scala
+++ liste.scala
@@ -3,10 +3,14 @@ data una lista l e un elemento x, restituisce una lista che contiene gli indici
 i tali che l(i) == x.
 Se la lista non contiene x, il metodo restituisce una lista vuota */
 
-
 /* Scrivere un metodo Scala isSorted che, data una lista di interi, 
 verifica se è ordinata in modo non decrescente. */
 
+/* Scrivere un metodo Scala consecutivi che, data una lista generica di n elementi, 
+restituisce la lista di n-1 coppie che contiene tutti gli elementi
+consecutivi della lista di ingresso. Ad esempio, se in ingresso viene
+dato List(3,6,5,7), il metodo deve produrre List((3,6),(6,5),(5,7)).
+Se la lista di ingresso contiene meno di due elementi, la funzione deve produrre la lista vuota. */
 
 @main def run() =
     l = List(2,5,6,7,3)
