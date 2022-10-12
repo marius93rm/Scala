dee518863bef68927bb11af41716abc072c3ca44
marius93rm <marius93rm@gmail.com>
2022-10-12 10:09:10 +0200

    Liste

--- cesare.scala
+++ cesare.scala
@@ -1,12 +1,39 @@
 def criptaCesare(frase: String, key: Int): String = {
     val dizionario = ('a','b','c','d','e','f','g','h','i','l','m','n','o','p','q','r','s','t','u','v','z')
+    var fraseCifrata = ""
+     
+    for (i <- 0 until frase.length()){
+        frase.charAt(i)
+        //cerca la lettera nella lista
+        for(j <- 0 until dizionario.length()){
+            if(frase.charAt(i) == dizionario(j)){
+                fraseCifrata += dizionario((j+key)%dizionario.length())
+            }
+        }
+    }
+
+    return fraseCifrata
 
 }
 
 def decriptaCesare(frase: String, key: Int): String = {
     val dizionario = ('a','b','c','d','e','f','g','h','i','l','m','n','o','p','q','r','s','t','u','v','z')
+    var fraseDecifrata = ""
 
+    for (i <- 0 until frase.length()){
+        frase.charAt(i)
+        //cerca la lettera nella lista
+        for(j <- 0 until dizionario.length()){
+            if(frase.charAt(i) == dizionario(j)){
+                fraseDecifrata += dizionario((j-key)%dizionario.length())
+            }
+        }
+    }
 
+}
+
+def decriptCesar(frase: String, key: Int): String = {
+    frase.
 }
 
 @main def run() =
@@ -23,6 +50,8 @@ def decriptaCesare(frase: String, key: Int): String = {
     println(chiaro)
     print("Frase criptata: ")
     println(criptaCesare(chiaro, chiave))
+
+    decriptCesar("DZZDFFDUHLONNUUNGAFNENONLDOONDOODRUDVHVZD", 3)
 
 
 
---
+++ liste.scala
@@ -0,0 +1,7 @@
+/* Scrivere un metodo Scala findIndices che, 
+data una lista l e un elemento x, restituisce una lista che contiene gli indici
+i tali che l(i) == x.
+Se la lista non contiene x, il metodo restituisce una lista vuota */
+
+@main def run() =
+    l = List(2,5,6,7,3)
