b3d5dd1c87d8f8a75bb0de39e2525cc6211df3f1
marius93rm <marius93rm@gmail.com>
2022-10-12 10:15:29 +0200

    map & reduce

--- liste.scala
+++ liste.scala
@@ -12,5 +12,10 @@ consecutivi della lista di ingresso. Ad esempio, se in ingresso viene
 dato List(3,6,5,7), il metodo deve produrre List((3,6),(6,5),(5,7)).
 Se la lista di ingresso contiene meno di due elementi, la funzione deve produrre la lista vuota. */
 
+/* Scrivere un metodo Scala max che, data una lista di stringhe,
+restituisce la lunghezza della stringa più lunga.
+Suggerimento: usare map e reduce
+*/
+
 @main def run() =
     l = List(2,5,6,7,3)
