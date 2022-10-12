f245e8315c8235360c0371f80308308bc8fdcd3e
marius93rm <marius93rm@gmail.com>
2022-10-12 10:13:31 +0200

    list2

--- liste.scala
+++ liste.scala
@@ -3,5 +3,10 @@ data una lista l e un elemento x, restituisce una lista che contiene gli indici
 i tali che l(i) == x.
 Se la lista non contiene x, il metodo restituisce una lista vuota */
 
+
+/* Scrivere un metodo Scala isSorted che, data una lista di interi, 
+verifica se è ordinata in modo non decrescente. */
+
+
 @main def run() =
     l = List(2,5,6,7,3)
